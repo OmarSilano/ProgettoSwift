30653fc17443ddd8049d5e4e2a90067e7ef64419
Omar Silano <silanoomar@gmail.com>
2025-07-04 14:33:51 +0200

    Initial Commit

---
+++ ProgettoSwift/ProgettoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgettoSwiftApp.swift
+//  ProgettoSwift
+//
+//  Created by Studente on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ProgettoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProgettoSwiftTests/ProgettoSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProgettoSwiftTests.swift
+//  ProgettoSwiftTests
+//
+//  Created by Studente on 04/07/25.
+//
+
+import Testing
+@testable import ProgettoSwift
+
+struct ProgettoSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
