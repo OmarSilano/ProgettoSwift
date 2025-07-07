164ac86cb8f6fa964cc375bf85518bda02463884
Omar Silano <silanoomar@gmail.com>
2025-07-07 11:56:45 +0200

    exploreWorkoutView

---
+++ ProgettoSwift/View/Explore/ExploreWorkoutsView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreWorkoutsView.swift
+//  ProgettoSwift
+//
+//  Created by Studente on 07/07/25.
+//
+
+import SwiftUI
+
+struct ExploreWorkoutsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreWorkoutsView()
+}
