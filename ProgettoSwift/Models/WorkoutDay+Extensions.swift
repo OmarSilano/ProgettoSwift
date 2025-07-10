ec9d88d105755197dda21c58912277d81ce67ef5
Carmine Terracciano <carminet2004@gmail.com>
2025-07-10 16:14:21 +0200

    fine db (per ora)

--- ProgettoSwift/Models/WorkoutDay+Extensions.swift
+++ ProgettoSwift/Models/WorkoutDay+Extensions.swift
@@ -8,28 +8,48 @@ import Foundation
 import CoreData
 
 extension WorkoutDay {
-    
-    // muscles come array di stringhe, con default empty array se nil
-    var musclesList: [String] {
+
+    var musclesList: [MuscleGroup] {
         get {
-            muscles as? [String] ?? []
+            (muscles as? [String])?.compactMap { MuscleGroup(rawValue: $0) } ?? []
         }
         set {
-            muscles = newValue as NSObject
+            muscles = newValue.map { $0.rawValue } as NSObject
         }
     }
+
+    // ricalcola i gruppi muscolari allenati
+    func updateMusclesFromDetails() {
+        // Prendi i dettagli associati al giorno di allenamento
+        guard let details = workoutDayDetail as? Set<WorkoutDayDetail> else {
+            musclesList = []
+            return
+        }
+
+        // Raccogli i muscoli degli esercizi, se validi
+        let muscles = details.compactMap { $0.exercise?.muscle }
+
+        // Converti le stringhe in enum MuscleGroup
+        let groups = muscles.compactMap { MuscleGroup(rawValue: $0) }
+
+        // Rimuovi duplicati e ordina
+        let uniqueSorted = Array(Set(groups)).sorted { $0.rawValue < $1.rawValue }
+
+        // Salva nella variabile musclesList
+        musclesList = uniqueSorted
+    }
+
     
-    // convenience init
     convenience init(context: NSManagedObjectContext,
                      id: UUID = UUID(),
+                     name: String,
                      isCompleted: Bool = false,
-                     muscles: [String] = [],
                      workout: Workout) {
         self.init(context: context)
         self.id = id
+        self.name = name
         self.isCompleted = isCompleted
-        self.musclesList = muscles
         self.workout = workout
+        self.musclesList = [] // inizialmente vuoto
     }
 }
-
