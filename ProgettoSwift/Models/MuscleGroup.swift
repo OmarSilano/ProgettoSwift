0d9010031390dd663f025dff1b282ef448b72523
Carmine Terracciano <carminet2004@gmail.com>
2025-07-10 12:05:14 +0200

    metodi per ogni muscolo

--- ProgettoSwift/Models/MuscleGroup.swift
+++ ProgettoSwift/Models/MuscleGroup.swift
@@ -14,3 +14,28 @@ enum MuscleGroup: String, CaseIterable, Codable{
     case shoulders = "Shoulders"
     case legs = "Legs"
 }
+
+extension MuscleGroup {
+    var availableMethods: [Method] {
+        switch self {
+            
+        case .chest:
+            return [.pushChest, .pullChest]
+            
+        case .back:
+            return [.pullUpBack, .rowBack]
+            
+        case .abs:
+            return [.upperAbs, .lowerAbs]
+            
+        case .arms:
+            return [.bicepsArms, .tricepsArms]
+            
+        case .shoulders:
+            return [.pushShoulders, .lateralRaiseShoulders, .rotationShoulders]
+            
+        case .legs:
+            return [.hPushLegs, .vPushLegs, .calfLegs, .hamstringLegs]
+        }
+    }
+}
